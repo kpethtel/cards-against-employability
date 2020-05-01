12113eefee28e948af8f1663cc85369442dc6268
kpethtel <kris.pethtel@gmail.com>
2020-04-30 23:47:42 -0400

    make gif fetcher

---
+++ frontend/src/actions/get_gif.js
@@ -0,0 +1,11 @@
+const GIPHY_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const GIPHY_URL = process.env.REACT_APP_GIPHY_URL;
+
+export const getGif = (searchTerm, setterCallback) => {
+  fetch(`${GIPHY_URL}?q=${searchTerm.replace(/\s/g, '+')}&api_key=${GIPHY_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      const gifs = result.data.map(gif => gif.images.downsized.url)
+      setterCallback(gifs)
+    })
+}
